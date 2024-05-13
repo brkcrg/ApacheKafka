be72cdf680dcaf24f39a7dadb69e9c2c0bee08ed
brkcrg <ciragburak@gmail.com>
2024-05-13 20:21:56 +0300

    first step create topic

---
+++ example-1/topic.js
@@ -0,0 +1,40 @@
+const {Kafka} =require("kafkajs");
+require('dotenv').config(); // dotenv modülünü yükleyin ve .env dosyasındaki değerleri yükleyin
+
+const dbHost = process.env.HOST;
+const broker =`${dbHost}:9092`;
+createTopic();
+
+async function createTopic() {
+   try {
+         //Admin Stuff
+    const kafka = new Kafka({
+        clientId:"Kafka_example_1",
+        brokers:[broker]
+    })
+
+    const admin = kafka.admin();
+    console.log("Kafka Broker'a bağlanıyor...");
+    await admin.connect();
+    console.log("Kafka Broker'a bağlantı başarılı,Topic üretilecek");
+    await admin.createTopics({
+        topics:[
+            {
+                topic:"Logs",
+                numPartitions:1
+            },
+            {
+                topic:"Logs2",
+                numPartitions:2
+            }
+        ]
+    });
+    console.log("Topic başarılı bir şekilde oluşturulmuştur..");
+    await admin.disconnect();
+   } catch (error) {
+        console.log("bir hata oluştu",error)
+   }
+   finally{
+    process.exit(0);
+   }
+}
