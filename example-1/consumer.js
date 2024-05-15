8213c35f2a15a9a5a4de6333388904fc11f67828
brkcrg <ciragburak@gmail.com>
2024-05-15 21:17:17 +0300

    create consumer connect and run codes

---
+++ example-1/consumer.js
@@ -0,0 +1,38 @@
+const {Kafka} =require("kafkajs");
+require('dotenv').config(); // dotenv modülünü yükleyin ve .env dosyasındaki değerleri yükleyin
+
+const dbHost = process.env.HOST;
+const broker =`${dbHost}:9092`;
+createConsumer();
+
+async function createConsumer() {
+   try {
+    const kafka = new Kafka({
+        clientId:"Kafka_example_1",
+        brokers:[broker]
+    })
+
+    const consumer = kafka.consumer({
+        groupId:"example_1_cg_1"
+    });
+    console.log("Consumera bağlanıyor...");
+    await consumer.connect();
+    console.log("Bağlantı başarılı");
+   
+    //Consumer subscribe...
+    await consumer.subscribe({
+        topic:"Logs",
+        fromBeginning:"true" //başlangıçtan başla
+
+    });
+
+    await consumer.run({
+        eachMessage: async result =>{
+            console.log(`${result.message.value}: Partition : => ${result.partition}`)//producer içerisindeki mesajı alıyoruz
+        }
+    });
+   } catch (error) {
+        console.log("bir hata oluştu",error)
+   }
+   
+}
